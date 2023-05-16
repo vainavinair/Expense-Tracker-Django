24eb4dd11cfa40b584ba9ac0c8ec30b0162a3b57
VanV <vainavinair2009558@gmail.com>
2023-05-16 15:58:24 +0530

    created models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    description = models.TextField(null=True, blank=True)
+    amount = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) 
+    created_at = models.DateTimeField(auto_now_add=True) 
